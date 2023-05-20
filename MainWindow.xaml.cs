d73bdfadb3c64fa6aecad85df13f6c9928ed633e
Chaitanya Malani <chaitanyamalani.rns@gmail.com>
2023-05-20 21:49:38 +0530

    Clean prebuild code

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,19 +21,11 @@ using Windows.Foundation.Collections;
 
 namespace ERP
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainWindow : Window
     {
         public MainWindow()
         {
             this.InitializeComponent();
-        }
-
-        private void myButton_Click(object sender, RoutedEventArgs e)
-        {
-            myButton.Content = "Clicked";
         }
     }
 }
